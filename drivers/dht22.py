38d9fc742ee32d835d3ac5814d7942e99852555e
przemeks <2173677+sosprz@users.noreply.github.com>
2024-01-14 00:55:47 +0100

    up

--- drivers/dht22.py
+++ drivers/dht22.py
@@ -1,17 +1,12 @@
 import time
 import board
 import adafruit_dht
-import sys, os
-dir=(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', '..','..','..')))
-sys.path.append(dir+'/app')
-from local_nettemp import insert
 
-#sensor = Adafruit_DHT.DHT22
+from nettemp import insert2
 
-name = os.listdir(dir+'/data/sensors/dht22/')
-for i in name:
-  pin = os.listdir(dir+'/data/sensors/dht22/'+i)[0]
-  #humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+
+pin
 
   pin = 'D'+pin
   dht_device = adafruit_dht.DHT22(getattr(board, pin))
@@ -24,14 +19,9 @@ for i in name:
     rom = 'dht22_temp_gpio_'+i
     type = 'temp'
     name = rom
-    data=insert(rom, type, value, name)
-    data.request()
+
    
     value = '{0:0.1f}'.format(humidity)
     rom = 'dht22_humid_gpio_'+i
     type = 'humid'
     name = rom
-    data=insert(rom, type, value, name)
-    data.request()
-   
-    
--- drivers/w1_kernel.py
+++ drivers/w1_kernel.py
@@ -1,19 +1,23 @@
-import requests, socket
-import sys, os, random, json
-from nettemp import insert
+import socket, random
+from nettemp import insert2
 
 def w1_kernel():
+    print ("w1_kernel")
     try:
-        print ("w1_kernel")
         from w1thermsensor import W1ThermSensor
         group = socket.gethostname()
+        data = []
+
         for sensor in W1ThermSensor.get_available_sensors():
             r = random.randint(10000,99999)
             value = sensor.get_temperature()
             rom = group+'_28_'+sensor.id
             type = 'temp'
             name = 'DS18b20-'+str(r)
-            data=insert(rom, type, value, name, group)
-            data.request()
+            data.append({"rom":rom,"type":type, "value":value,"name":name, "group":group})
+
+        data=insert2(data)
+        data.request()
+
     except:
         print ("No w1_kernel")
