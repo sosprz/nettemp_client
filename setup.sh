504ffc58eed9ef12402731a7d9098c6c0acd0413
przemeks <2173677+sosprz@users.noreply.github.com>
2024-01-13 15:16:47 +0100

    add packages to pip

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ sudo apt-get -y install python3-pip python3-venv
 python3 -m venv venv
 . venv/bin/activate
 pip3 install w1thermsensor requests apscheduler pyaml psutil smbus gpiozero
+pip3 install git+https://github.com/nicmcd/vcgencmd.git
+pip3 install adafruit-circuitpython-htu21d adafruit-circuitpython-tsl2561 Adafruit-BMP adafruit-circuitpython-adxl34x adafruit-circuitpython-dht
 
 deactivate
 
