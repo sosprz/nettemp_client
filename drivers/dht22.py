b4ce4d09c1c38b65efce0640e022acad3fbcab67
przemeks <2173677+sosprz@users.noreply.github.com>
2024-01-14 13:27:50 +0100

    up

--- drivers/dht22.py
+++ drivers/dht22.py
@@ -6,31 +6,31 @@ from nettemp import insert2
 
 def dht22():
   print("DHT22")
-  try:
-    pin = 4
-    pin = 'D'+pin
-    dht_device = adafruit_dht.DHT22(getattr(board, pin))
-    temperature = dht_device.temperature
-    humidity = dht_device.humidity
-    print(temperature)
-    group = socket.gethostname()
-    data = []
+  #try:
+  pin = 4
+  pin = 'D'+pin
+  dht_device = adafruit_dht.DHT22(getattr(board, pin))
+  temperature = dht_device.temperature
+  humidity = dht_device.humidity
+  print(temperature)
+  group = socket.gethostname()
+  data = []
 
-    if humidity is not None and temperature is not None:
-      value = '{0:0.1f}'.format(temperature)
-      rom = group+'_dht22_temp_gpio_'+i
-      type = 'temp'
-      name = rom
-      data.append({"rom":rom,"type":type, "value":value,"name":name, "group":group})
-    
-      value = '{0:0.1f}'.format(humidity)
-      rom = group+'_dht22_humid_gpio_'+i
-      type = 'humid'
-      name = rom
-      data.append({"rom":rom,"type":type, "value":value,"name":name, "group":group})
+  if humidity is not None and temperature is not None:
+    value = '{0:0.1f}'.format(temperature)
+    rom = group+'_dht22_temp_gpio_'+i
+    type = 'temp'
+    name = rom
+    data.append({"rom":rom,"type":type, "value":value,"name":name, "group":group})
+  
+    value = '{0:0.1f}'.format(humidity)
+    rom = group+'_dht22_humid_gpio_'+i
+    type = 'humid'
+    name = rom
+    data.append({"rom":rom,"type":type, "value":value,"name":name, "group":group})
 
-      data=insert2(data)
-      data.request()
+    data=insert2(data)
+    data.request()
 
-  except:
-    print("No DHT22")
+  #except:
+  #  print("No DHT22")
