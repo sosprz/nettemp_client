84993cab20a622d16be89803d582f8027e6c4c94
przemeks <2173677+sosprz@users.noreply.github.com>
2024-01-16 19:53:57 +0100

    up

--- nettemp_client.py
+++ nettemp_client.py
@@ -12,7 +12,7 @@ sched = BackgroundScheduler({'apscheduler.timezone': 'Europe/London'})
 def config_remote_config():
   config_file = open("config.conf")
   config = yaml.load(config_file, Loader=yaml.FullLoader)
-  if config["remote_config"]:
+  if config["remote_config"]["enabled"]:
     return True
   else:
     return False
