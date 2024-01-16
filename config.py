eba342e806744c67e0a33b8117eb3862262fc0de
przemeks <2173677+sosprz@users.noreply.github.com>
2024-01-16 21:44:55 +0100

    up

--- config.py
+++ config.py
@@ -2,12 +2,12 @@ import os, yaml
 
 def configInit():
     configd = {
-         'w1_kernel': {'enabled': False, 'read_in_sec': 60, 'info': 'DS18B20 over USB DS9490R, I2C DS2482'}, 
+         'w1_kernel': {'enabled': True, 'read_in_sec': 60, 'info': 'DS18B20 over USB DS9490R, I2C DS2482'}, 
          'w1_kernel_gpio': {'enabled': False, 'read_in_sec': 60, 'info':'gpio4, turn off w1_kernel and do system reboot. Add dtoverlay=w1-gpio,gpiopin=x to /boot/config.txt, do system reboot.'}, 
          'system': {'enabled': True, 'read_in_sec': 60}, 
          'tmp102': {'enabled': False, 'read_in_sec': 60}, 
          'rpi': {'enabled': False, 'read_in_sec': 60}, 
-         'lm_sensors': {'enabled': False, 'read_in_sec': 60},
+         'lm_sensors': {'enabled': True, 'read_in_sec': 60},
          'bmp180': {'enabled': False, 'read_in_sec': 60}, 
          'bme280': {'enabled': False, 'read_in_sec': 60}, 
          'htu21d': {'enabled': False, 'read_in_sec': 60}, 
@@ -19,7 +19,7 @@ def configInit():
          'mpl3115a2': {'enabled': False, 'read_in_sec': 60}, 
          'dht11': {'enabled': False, 'read_in_sec': 60, 'gpio_pin': 4, 'info':'minimum read interval 60sec'}, 
          'dht22': {'enabled': False, 'read_in_sec': 60, 'gpio_pin': 4, 'info':'minimum read interval 60sec'},
-         'ping' : {'enabled': False, 'read_in_sec': 60, 'hosts':['wp.pl','google.com']}
+         'ping' : {'enabled': True, 'read_in_sec': 60, 'hosts':['wp.pl','google.com']}
         }
 
     config = {
