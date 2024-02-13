ec30f1978519664312ed71e3bd4546cb577e33c7
przemeks <2173677+sosprz@users.noreply.github.com>
2024-02-13 06:49:09 +0100

    up

--- drivers/ds2482.py
+++ drivers/ds2482.py
@@ -1,34 +1,30 @@
-import os
 import subprocess
 import time
+import os
 
-# Check for the existence of i2c devices and assign nbus accordingly
-if os.path.exists('/dev/i2c-0'):
-    nbus = "i2c-0"
-elif os.path.exists('/dev/i2c-1'):
-    nbus = "i2c-1"
-elif os.path.exists('/dev/i2c-2'):
-    nbus = "i2c-2"
-elif os.path.exists('/dev/i2c-3'):
-    nbus = "i2c-3"
-else:
-    nbus = None
+# Assuming the initial setup
+nbus = None
+for i in range(4):  # Check for the first available i2c bus
+    if os.path.exists(f'/dev/i2c-{i}'):
+        nbus = f"i2c-{i}"
+        break
 
 if nbus is not None:
-    # Load kernel modules
-    subprocess.run(['modprobe', 'ds2482'])
-    subprocess.run(['modprobe', 'w1-therm'])
+    # Run commands with 'sudo'
+    subprocess.run(['sudo', 'modprobe', 'ds2482'], check=True)
+    subprocess.run(['sudo', 'modprobe', 'w1-therm'], check=True)
     
     # Give time for the modules to load
     time.sleep(3)
     
-    # Write to the new_device file for each address
+    # Prepare the commands for writing to new_device files
     addresses = ['0x18', '0x19', '0x1a', '0x1b']
     for address in addresses:
+        # This part requires the script to be run with sudo for file access permissions
         with open(f'/sys/bus/i2c/devices/{nbus}/new_device', 'w') as f:
             f.write(f'ds2482 {address}')
         time.sleep(3)
     
-    # Set w1_master_pullup
+    # Set w1_master_pullup - also requires sudo to run the script
     with open('/sys/bus/w1/devices/w1_bus_master1/w1_master_pullup', 'w') as f:
         f.write('0')
