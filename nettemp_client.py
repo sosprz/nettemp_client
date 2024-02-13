b02a65d1c7136606efe84e63571f263989233175
przemeks <2173677+sosprz@users.noreply.github.com>
2024-02-13 09:10:10 +0100

    up

--- nettemp_client.py
+++ nettemp_client.py
@@ -31,7 +31,7 @@ def load_yaml(file_path):
     try:
         with open(file_path, 'r') as file:
             return yaml.safe_load(file)
-    except yaml.YAMLError as e:
+    except:
         #logging.error(f"Error loading YAML file {file_path}: {e}")
         return None
 
