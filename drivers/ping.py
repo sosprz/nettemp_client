2c952f85a65987e650996b5d3427d8d3a2973b96
przemeks <2173677+sosprz@users.noreply.github.com>
2024-02-13 11:03:37 +0100

    up

--- drivers/ping.py
+++ drivers/ping.py
@@ -5,6 +5,8 @@ import pingparsing, requests, time
 requests.packages.urllib3.disable_warnings()
 import logging
 
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
 
 def ping():
     logging.info("[  nettemp client  ][ ping ] start")
--- nettemp.py
+++ nettemp.py
@@ -3,6 +3,8 @@ requests.packages.urllib3.disable_warnings()
 import yaml, json, os
 import logging
 
+logging.basicConfig(level=logging.DEBUG, format='[%(asctime)s] %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
 
 class insert:
   def __init__(self, rom, type, value, name):
