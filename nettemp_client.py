b3f35eadbee237cb08dcb6c237ecd72ec179d80d
przemeks <2173677+sosprz@users.noreply.github.com>
2024-02-13 08:48:31 +0100

    up

--- nettemp_client.py
+++ nettemp_client.py
@@ -11,7 +11,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from requests.exceptions import RequestException
 
 # Setup logging
-logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+logging.basicConfig(level=logging.DEBUG, format='[%(asctime)s] %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
 # Constants
 CONFIG_MAIN = "config.conf"
