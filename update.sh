5e48670ee3c822197f2eebeb98f7ea01c58f0c74
przemeks <2173677+sosprz@users.noreply.github.com>
2024-02-13 08:23:55 +0100

    up

--- update.sh
+++ update.sh
@@ -2,8 +2,7 @@ git pull
 python3 -m venv venv
 . venv/bin/activate
 pip3 install -r requirements.txt
-deactivate
-
 python3 config.py
+deactivate
 pkill -f nettemp_client.py
 /bin/nohup $(pwd)/venv/bin/python3 $(pwd)/nettemp_client.py &
