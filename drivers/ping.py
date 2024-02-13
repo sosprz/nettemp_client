d9c291942d79444982eb895987f294ab4b02874b
przemeks <2173677+sosprz@users.noreply.github.com>
2024-02-13 22:30:05 +0100

    up

--- drivers/ping.py
+++ drivers/ping.py
@@ -1,70 +1,81 @@
-import yaml, socket
 from nettemp import insert2
-import json
-import pingparsing, requests, time
-requests.packages.urllib3.disable_warnings()
 import logging
+import yaml
+import pingparsing
+import requests
+import time
+import json
+from concurrent.futures import ThreadPoolExecutor
+requests.packages.urllib3.disable_warnings()
 
-logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+def perform_ping(name):
+    ping_parser = pingparsing.PingParsing()
+    transmitter = pingparsing.PingTransmitter()
+    data = None
 
+    if name.startswith(('http://', 'https://')):
+        start = time.perf_counter()
+        try:
+            r = requests.get(name, verify=False, timeout=5)
+            code = r.status_code
+        except:
+            code = 0
+
+        request_time = time.perf_counter() - start
+
+        if code == 200:
+            value = '{0:0.2f}'.format(request_time)
+        else:
+            value = 0
+        type = 'url'
+
+    else:
+        transmitter.destination = name
+        transmitter.count = 3
+        result = transmitter.ping()
+        out = json.dumps(ping_parser.parse(result).as_dict(), indent=4)
+        jout = json.loads(out)
+
+        if jout['rtt_avg']:
+            value = jout['rtt_avg']
+            value = '{0:0.2f}'.format(value)
+        else:
+            value = 0
+        type = 'host'
+
+    name = name.replace("https://", "").replace("http://", "")
+    rom = '_' + name
+    if value != 0:
+        data = {"rom": rom, "type": type, "value": value, "name": name}
+        logging.info(f"[  nettemp client  ][ ping ] {name} Request completed in {value}ms")
+    else:
+        logging.info(f"[  nettemp client  ][ ping ] {name} no connection!")
+
+    return data
 
 def ping():
     logging.info("[  nettemp client  ][ ping ] start")
 
-    
-    data = []
-
     try:
         config = yaml.load(open("remote.conf"), Loader=yaml.FullLoader)
-    except:
-        config = yaml.load(open("drivers.conf"), Loader=yaml.FullLoader)
+    except Exception as e:
+        logging.error(f"Failed to load configuration: {e}")
+        config = yaml.load(open("local.conf"), Loader=yaml.FullLoader)
 
-    #print(config["ping"]["hosts"])
+    hosts = config["ping"]["hosts"]
 
-    ping_parser = pingparsing.PingParsing()
-    transmitter = pingparsing.PingTransmitter()
-    for name in config["ping"]["hosts"]:
-        
-        if name.startswith(('http://', 'https://')):
-            start = time.perf_counter()
-            try:
-                r = requests.get(name, verify=False, timeout=5)
-                code = r.status_code
-            except:
-                code = 0
-                
-            request_time = time.perf_counter() - start
-            
-            if code == 200: 
-                value = '{0:0.2f}'.format(request_time)
-            else:
-                value = 0
-            type='url'
-        
-        else:
-            transmitter.destination = name
-            transmitter.count = 3
-            result = transmitter.ping()
-            out = json.dumps(ping_parser.parse(result).as_dict(), indent=4)
-            jout = json.loads(out)
-    
-            if jout['rtt_avg']:
-                value = jout['rtt_avg']
-                value = '{0:0.2f}'.format(value)
-            else:
-                value = 0
-            type='host'
-         
+    # Use ThreadPoolExecutor to perform ping in parallel
+    with ThreadPoolExecutor(max_workers=len(hosts)) as executor:
+        results = list(executor.map(perform_ping, hosts))
 
-        name = name.replace("https://","")
-        name = name.replace("http://","")
-        rom = '_'+name
-        if value != 0:
-            data.append({"rom":rom,"type":type,"value":value,"name":name})
-            logging.info(f"[  nettemp client  ][ ping ] {name} Request completed in {value}ms")
-        else:
-            logging.info(f"[  nettemp client  ][ ping ] {name} no connection!")
+    # Filter out None results if any
+    data = [result for result in results if result is not None]
 
-    data=insert2(data)
-    data.request()
+    if data:
+        # Assuming insert2 is a function that processes the data list
+        # And assuming data.request() should be calling a method on the result of insert2
+        processed_data = insert2(data)
+        processed_data.request()
+
     logging.info("[  nettemp client  ][ ping ] End")
+
